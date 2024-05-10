a8d0ce6e6d193858cf239bb6e3ceca251e358b39
OctoD <OctoD@users.noreply.github.com>
2024-05-10 17:20:51 +0200

    chore: starts working on the ui of the inspector plugin

--- src/editor/inspector_plugins/tag_dictionary_editor_inspector_plugin.rs
+++ src/editor/inspector_plugins/tag_dictionary_editor_inspector_plugin.rs
@@ -1,4 +1,5 @@
-use godot::engine::{EditorInspectorPlugin, IEditorInspectorPlugin, Tree};
+use godot::engine::control::{LayoutPreset, SizeFlags};
+use godot::engine::{EditorInspectorPlugin, HBoxContainer, IEditorInspectorPlugin, Label, Tree, TreeItem, VBoxContainer};
 use godot::prelude::*;
 
 use crate::tag_dictionary::TagDictionary;
@@ -25,23 +26,44 @@ impl IEditorInspectorPlugin for TagDictionaryEditorInspectorPlugin {
         _usage_flags: godot::engine::global::PropertyUsageFlags,
         _wide: bool,
     ) -> bool {
+        if _name == "RefCounted".into() {
+            return false;
+        }
+
+        let title_label = Label::new_alloc();
+        let mut container = VBoxContainer::new_alloc();
+        let mut header = HBoxContainer::new_alloc();
         let mut tree = Tree::new_alloc();
         let tag_dictionary = _object
             .try_cast::<TagDictionary>()
             .expect("Failed to cast to TagDictionary");
 
+        header.add_child(title_label.to_variant().to());
+
+        container.add_child(header.to_variant().to());
+        container.add_child(tree.to_variant().to());
+
+        let mut tags = tag_dictionary.bind().get_tags();
+
+        tags.sort();
+
+        tree.set_anchors_and_offsets_preset(LayoutPreset::FULL_RECT);
+        tree.set_column_title(0, "Tag name".into());
+        tree.set_custom_minimum_size(Vector2::new(0.0, 200.0));
+        tree.set_hide_root(true);
+        tree.set_v_size_flags(SizeFlags::EXPAND_FILL);
+        
+        // done this because of this https://github.com/godot-rust/gdext/issues/156
+        let mut root: Gd<TreeItem> = tree.call("create_item".into(), &[]).to();
+
+        for tag in tags.as_slice() {
+            let mut item: Gd<TreeItem> = root.call("create_child".into(), &[]).to();
+
+            item.set_text(0, tag.clone());
+        }
+
         self.to_gd()
-            .add_custom_control(tree.to_variant().to::<Gd<godot::engine::Control>>());
-
-        let tags = tag_dictionary.bind().get_tags();
-
-        // let mut root = tree.create_item().expect("Root item not created");
-
-        // for tag in tags.as_slice() {
-        //     let mut item = root.create_child().expect("Child item not created");
-
-        //     item.set_text(0, tag.clone());
-        // }
+            .add_custom_control(container.to_variant().to());
 
         true
     }
