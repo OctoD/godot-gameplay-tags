c4abd23ec9442ab6982991a881d2172665bb45f9
OctoD <iamctod@gmail.com>
2024-05-13 12:45:32 +0200

    feat: adds set_tags

--- src/tag_manager.rs
+++ src/tag_manager.rs
@@ -19,6 +19,7 @@ impl TagManager {
 		return PackedStringArray::new();
 	}
 
+	/// Adds a tag to a target node.
 	#[func]
 	pub fn add_tag(&self, tag: GString, mut target: Gd<Node>) {
 		let mut packed_string_array = self._get_tags(&target);
@@ -30,6 +31,7 @@ impl TagManager {
 		}
 	}
 
+	/// Adds multiple tags to a target node.
 	#[func]
 	pub fn add_tags(&self, tags: PackedStringArray, mut target: Gd<Node>) {
 		let mut packed_string_array = self._get_tags(&target);
@@ -48,16 +50,19 @@ impl TagManager {
 		target.set_meta(self.get_meta_name(), packed_string_array.to_variant());
 	}
 	
+	/// Returns the group name used to store tagged nodes.
 	#[func]
 	pub fn get_group_name(&self) -> StringName {
 		StringName::from_str("octod_ggs_tagged_node_group").expect("Failed to create StringName")
 	}
 	
+	/// Returns the meta name used to store tags.
 	#[func]
 	pub fn get_meta_name(&self) -> StringName {
 		StringName::from_str("octod_ggs_tagged_node").expect("Failed to create StringName")
 	}
 
+	/// Returns all tagged nodes descending from a target node.
 	#[func]
 	pub fn get_tagged_nodes(&self, target: Gd<Node>) -> Array<Gd<Node>> {
 		if let Some(mut tree) = target.get_tree() {
@@ -67,11 +72,13 @@ impl TagManager {
 		return Array::new();
 	}
 	
+	/// Returns all tags of a target node.
 	#[func]
 	pub fn get_tags(&self, target: Gd<Node>) -> PackedStringArray {
 		return self._get_tags(&target);
 	}
 
+	/// Checks if a target node has a specific tag.
 	#[func]
 	fn has_tag(&self, tag: GString, target: Gd<Node>) -> bool {
 		return self.
@@ -79,6 +86,7 @@ impl TagManager {
 			.contains(tag)
 	}
 
+	/// Checks if a target node has all of the specified tags.
 	#[func]
 	pub fn has_all_tags(&self, tags: PackedStringArray, target: Gd<Node>) -> bool {
 		return self
@@ -88,6 +96,7 @@ impl TagManager {
 			.all(|tag| tags.contains(tag.clone()))
 	}
 
+	/// Checks if a target node has any of the specified tags.
 	#[func]
 	pub fn has_some_tags(&self, tags: PackedStringArray, target: Gd<Node>) -> bool {
 		self
@@ -97,6 +106,7 @@ impl TagManager {
 			.any(|tag| tags.contains(tag.clone()))
 	}
 
+	/// Checks if a target node has none of the specified tags.
 	#[func]
 	pub fn has_none_tags(&self, tags: PackedStringArray, target: Gd<Node>) -> bool {
 		self
@@ -106,6 +116,7 @@ impl TagManager {
 			.all(|tag| !tags.contains(tag.clone()))
 	}
 
+	/// Removes a tag from a target node. If the node has no more tags, it will be removed from the tagged nodes group.
 	#[func]
 	pub fn remove_tag(&self, tag: GString, mut target: Gd<Node>) {
 		let mut packed_string_array = self._get_tags(&target);
@@ -121,6 +132,7 @@ impl TagManager {
 		}
 	}
 
+	/// Removes multiple tags from a target node. If the node has no more tags, it will be removed from the tagged nodes group.
 	#[func]
 	pub fn remove_tags(&self, tags: PackedStringArray, mut target: Gd<Node>) {
 		let mut packed_string_array = self._get_tags(&target);
@@ -136,6 +148,18 @@ impl TagManager {
 			target.remove_meta(self.get_meta_name());
 		} else {
 			target.set_meta(self.get_meta_name(), packed_string_array.to_variant());
+		}
+	}
+
+	/// Sets the tags of a target node. If the node has no more tags, it will be removed from the tagged nodes group.
+	#[func]
+	pub fn set_tags(&self, tags: PackedStringArray, mut target: Gd<Node>) {
+		if tags.len() == 0 {
+			target.remove_from_group(self.get_group_name());
+			target.remove_meta(self.get_meta_name());
+		} else {
+			target.add_to_group(self.get_group_name());
+			target.set_meta(self.get_meta_name(), tags.to_variant());
 		}
 	}
 }
